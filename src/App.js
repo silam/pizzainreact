24f923db3a43d691e81b3c9a24fefaa3dfc75999
silam <silam@hotmail.com>
2024-12-29 17:55:18 -0600

    pretier

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-const Pizza = (props)=>{
-    return React.createElement("div",{},[
-        React.createElement("h2",{}, props.name),
-        React.createElement("p",{}, props.description),
-
-    ]);
-}
-
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-            [
-                React.createElement("h1",{},"Pizza Serverless Company"),
-                React.createElement(Pizza, {name: "The Peppo", description: "With Pepper"}),
-                React.createElement(Pizza, {name: "American", description: "French Fries"}),
-                React.createElement(Pizza, {name: "Itaian", description: "Hot dog"}),
-                React.createElement(Pizza, {name: "Mexican Pizza", description: "Ketchup"})
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Pizza Serverless Company"),
 
-            ]
+    React.createElement(Pizza, {
+      name: "The Peppo",
 
-       
-    )
-}
+      description: "With Pepper",
+    }),
+    React.createElement(Pizza, {
+      name: "American",
+      description: "French Fries",
+    }),
+    React.createElement(Pizza, {
+      name: "Itaian",
+
+      description: "Hot dog",
+    }),
+    React.createElement(Pizza, {
+      name: "Mexican Pizza",
+      description: "Ketchup",
+    }),
+  ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
